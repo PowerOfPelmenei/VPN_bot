ae18de7fc67267144408cc5ad41da61cca0c5c76
PowerOfPelmenei <danilinclude@gmail.com>
2026-07-23 04:34:23 +0300

    change check

--- check_panel.py
+++ check_panel.py
@@ -1,9 +1,20 @@
-from py3xui import XUI
+from py3xui import Api
 
 # Подставьте свои данные
-xui = XUI("https://nl-panel.konoha.us.ci:30251/ae7kqDyXatmoYxj5KG", "Orochimaru", "Navuhodonosor_101")
+xui = Api("https://ваш_адрес_панели", "логин", "пароль")
 xui.login()
 
-# Проверяем, есть ли методы для подписок
-print("Доступные методы:", [m for m in dir(xui) if not m.startswith('_')])
-print("\nМетоды subscription:", [m for m in dir(xui.subscription) if not m.startswith('_')])
+# Проверяем доступные методы
+print("Доступные методы Api:", [m for m in dir(xui) if not m.startswith('_')])
+
+# Проверяем subscription (если есть)
+if hasattr(xui, 'subscription'):
+    print("Методы subscription:", [m for m in dir(xui.subscription) if not m.startswith('_')])
+else:
+    print("Свойство subscription отсутствует")
+
+# Проверяем client (если есть)
+if hasattr(xui, 'client'):
+    print("Методы client:", [m for m in dir(xui.client) if not m.startswith('_')])
+else:
+    print("Свойство client отсутствует")
